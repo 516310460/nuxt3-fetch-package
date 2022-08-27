3e288ae5cccc1a58f1a9b6418a590128bc4824ab
HomWang <ajake123123@gmail.com>
2022-08-27 10:50:32 +0800

    Add How to use

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <div>Welcome to Nuxt3</div>
     <ul>
       <li v-for="(item, index) in data" :key="index">
-        <a :href="item.url" target="_blank" rel="noopener noreferrer">{{item.title}}</a>
+        <a :href="item.url" target="_blank" rel="noopener noreferrer">{{ item.title }}</a>
       </li>
     </ul>
   </div>
@@ -11,11 +11,24 @@
 
 <script setup lang="ts">
 import { FeedsApi } from "./api/api";
+
+// 1.How to use
 const {
+  data,
+  pending,
+  refresh,
+  error
+} = await FeedsApi({ feed: 'news', page: 1 });
+
+// 2.How to use
+FeedsApi({ feed: 'news', page: 1 }).then(res => {
+  const {
     data,
     pending,
-    refresh
-} = await FeedsApi({ feed: 'news', page: 1 });
+    refresh,
+    error
+  } = res
+})
 </script>
 
 <style scoped>
